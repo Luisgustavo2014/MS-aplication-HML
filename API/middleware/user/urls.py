b749bbdbd9f73f604a624be953266340be323a7e
Patrick Piccini <patrickbpiccini@hotmail.com>
2022-02-25 00:01:47 -0300

    Alteração no framework de Djado para Flask visto que a aplicação somente necessita de uma API back-end sem qualquer pajina web. Foi criado as rotas de usuario e de order como solicitado no desafio do problema.

--- API/middleware/user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UserView
-
-
-urlpatterns = [
-    path('alluser/', UserView.search_user, name='alluser'),
-]
---
+++ API/server.py
@@ -0,0 +1,98 @@
+
+#          ──▒▒▒▒▒────▄████▄─────
+#          ─▒─▄▒─▄▒──███▄█▀──────
+#          ─▒▒▒▒▒▒▒─▐████──█──█──
+#          ─▒▒▒▒▒▒▒──█████▄──────
+#          ─▒─▒─▒─▒───▀████▀─────
+#   ꧁——————Patrick Piccini——————꧂
+
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+### User Routes
+@app.route("/user/create_user/", methods=['POST'])
+def create_user():
+    if request.method == 'POST':
+        return {'Status': 200, 'Message':'bem vindo'}
+    else:
+        return {'Status': 404, 'Message':'Erro no envio do method'}
+
+
+@app.route("/user/list_user/", methods=['GET'])
+def list_user():
+    if request.method == 'GET':
+        return {'Status': 200, 'Message':'bem vindo'}
+    else:
+        return {'Status': 404, 'Message':'Erro no envio do method'}
+
+
+@app.route("/user/display_user/", methods=['GET'])
+def display_user():
+    if request.method == 'GET':
+        return {'Status': 200, 'Message':'bem vindo'}
+    else:
+        return {'Status': 404, 'Message':'Erro no envio do method'}
+
+
+@app.route("/user/edit_user/", methods=['POST'])
+def edit_user():
+    if request.method == 'POST':
+        return {'Status': 200, 'Message':'bem vindo'}
+    else:
+        return {'Status': 404, 'Message':'Erro no envio do method'}
+
+
+@app.route("/user/delete_user/", methods=['DELETE'])
+def delete_user():
+    if request.method == 'DELETE':
+        return {'Status': 200, 'Message':'bem vindo'}
+    else:
+        return {'Status': 404, 'Message':'Erro no envio do method'}
+
+
+
+### Order Routes
+@app.route("/user/create_order/", methods=['POST'])
+def create_order():
+    if request.method == 'POST':
+        return {'Status': 200, 'Message':'bem vindo'}
+    else:
+        return {'Status': 404, 'Message':'Erro no envio do method'}
+
+
+@app.route("/user/list_order/", methods=['GET'])
+def list_order():
+    if request.method == 'GET':
+        return {'Status': 200, 'Message':'bem vindo'}
+    else:
+        return {'Status': 404, 'Message':'Erro no envio do method'}
+
+
+@app.route("/user/display_by_order/", methods=['GET'])
+def display_by_order():
+    if request.method == 'GET':
+        return {'Status': 200, 'Message':'bem vindo'}
+    else:
+        return {'Status': 404, 'Message':'Erro no envio do method'}
+
+
+@app.route("/user/edit_order/", methods=['POST'])
+def edit_user():
+    if request.method == 'POST':
+        return {'Status': 200, 'Message':'bem vindo'}
+    else:
+        return {'Status': 404, 'Message':'Erro no envio do method'}
+
+
+@app.route("/user/delete_prder/", methods=['DELETE'])
+def delete_user():
+    if request.method == 'DELETE':
+        return {'Status': 200, 'Message':'bem vindo'}
+    else:
+        return {'Status': 404, 'Message':'Erro no envio do method'}
+
+
+
+app.run('0.0.0.0', 7000)
