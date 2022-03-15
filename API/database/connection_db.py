3883d245bde8519edae3c800cac3dda607416020
Patrick Piccini <patrickbpiccini@hotmail.com>
2022-03-14 23:42:21 -0300

    alteração de create tables

--- API/database/connection_db.py
+++ API/database/connection_db.py
@@ -16,31 +16,32 @@ class ConnectionDatabase():
     
     def create_tables(self):
         self.cursor.execute("SELECT version();")
+        record = self.cursor.fetchone()
+        print("[X] You are connected to - ", record, "\n")
 
         create_table_query = '''
-CREATE TABLE IF NOT EXISTS users (
-    user_id SERIAL NOT NULL,
-    name varchar(50) NOT NULL,
-    cpf integer NOT NULL,
-    email varchar(50) NOT NULL,
-    phone_number integer NOT NULL,
-    created_at varchar(50),
-    updated_at varchar(50),
-    PRIMARY KEY (user_id)
-);
+            CREATE TABLE IF NOT EXISTS users (
+                user_id SERIAL NOT NULL,
+                name varchar(50) NOT NULL,
+                cpf integer NOT NULL,
+                email varchar(50) NOT NULL,
+                phone_number integer NOT NULL,
+                created_at varchar(50),
+                updated_at varchar(50),
+                PRIMARY KEY (user_id)
+            );
 
-CREATE TABLE IF NOT EXISTS orders (
-    order_id SERIAL NOT NULL,
-    user_id integer,
-    item_description integer NOT NULL,
-    item_quantity integer NOT NULL,
-    item_price integer NOT NULL,
-    PRIMARY KEY (order_id),
-    FOREIGN KEY(user_id) REFERENCES users(user_id)
-);'''
+            CREATE TABLE IF NOT EXISTS orders (
+                order_id SERIAL NOT NULL,
+                user_id integer,
+                item_description integer NOT NULL,
+                item_quantity integer NOT NULL,
+                item_price integer NOT NULL,
+                PRIMARY KEY (order_id),
+                FOREIGN KEY(user_id) REFERENCES users(user_id)
+            );'''
 
         self.cursor.execute(create_table_query)
         print('[X] Created tables')
 
-        record = self.cursor.fetchone()
-        print("[X] You are connected to - ", record, "\n")
+        
